5129a4c2f19af4f4f90bf0c24063705372336046
BlueIndio <acebedo.hidalgo.jason@gmail.com>
2023-05-31 20:15:37 +0200

    añadido nuevo campo para modelo Post

---
+++ GJM/Data/Migrations/20230531181142_ModPost.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GJM.Data.Migrations
+{
+    public partial class ModPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tittle",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tittle",
+                table: "Posts");
+        }
+    }
+}
--- GJM/Data/Post.cs
+++ GJM/Data/Post.cs
@@ -3,6 +3,7 @@
     public class Post
     {
         public int Id { get; set; }
+        public string Tittle { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public string Tags { get; set; } = string.Empty;
         public DateTime RegistrationDate { get; set; } = DateTime.MinValue;
